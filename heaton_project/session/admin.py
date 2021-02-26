5eb48024efda6400af5abbac5e85bc1da7ab2ba3
Ubuntu <ubuntu@ip-172-31-24-249.us-west-2.compute.internal>
2021-02-26 22:06:30 +0000

    update ordering of admin panel

--- heaton_project/session/admin.py
+++ heaton_project/session/admin.py
@@ -5,13 +5,19 @@ from .models import key, played_tune, name_yer_tune, tune_of_the_month
 # Create Ordering For Admin Panel
 class tune_admin(admin.ModelAdmin):
     ordering = ['name1']
+class session_admin(admin.ModelAdmin):
+    ordering = ['-date']
+class played_tune_group_admin(admin.ModelAdmin):
+    ordering = ['-session__date','-session_order_num']
+class played_tune_admin(admin.ModelAdmin):
+    ordering = ['-played_tune_group__session__date','-played_tune_group__session_order_num','-group_order_num']
 
 # Register your models here.
 admin.site.register(tune_type)
 admin.site.register(tune, tune_admin)
-admin.site.register(session)
-admin.site.register(played_tune_group)
+admin.site.register(session, session_admin)
+admin.site.register(played_tune_group, played_tune_group_admin)
 admin.site.register(key)
-admin.site.register(played_tune)
+admin.site.register(played_tune, played_tune_admin)
 admin.site.register(name_yer_tune)
 admin.site.register(tune_of_the_month)
