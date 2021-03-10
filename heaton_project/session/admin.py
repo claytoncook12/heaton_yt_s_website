32e20a34fc2c448b773e349c87aada9742ba9c9a
Ubuntu <ubuntu@ip-172-31-24-249.us-west-2.compute.internal>
2021-03-10 16:25:48 +0000

    update ordering of tunes and tune_types

--- heaton_project/session/admin.py
+++ heaton_project/session/admin.py
@@ -11,13 +11,17 @@ class played_tune_group_admin(admin.ModelAdmin):
     ordering = ['-session__date','-session_order_num']
 class played_tune_admin(admin.ModelAdmin):
     ordering = ['-played_tune_group__session__date','-played_tune_group__session_order_num','-group_order_num']
+class key_admin(admin.ModelAdmin):
+    ordering = ['key_type_char']
+class tune_type_admin(admin.ModelAdmin):
+    ordering = ['tune_type_char']
 
 # Register your models here.
-admin.site.register(tune_type)
+admin.site.register(tune_type, tune_type_admin)
 admin.site.register(tune, tune_admin)
 admin.site.register(session, session_admin)
 admin.site.register(played_tune_group, played_tune_group_admin)
-admin.site.register(key)
+admin.site.register(key, key_admin)
 admin.site.register(played_tune, played_tune_admin)
 admin.site.register(name_yer_tune)
 admin.site.register(tune_of_the_month)
